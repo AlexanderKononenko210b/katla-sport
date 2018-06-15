231681ffe7337354d6af4a34fe388541aa2352d2
Aliaksandr Kananenka <Aliaksandr_Kananenka@epam.com>
2018-06-15 20:11:45 +0300

    Add Id, Code, Name, IsDelete property in HiveSectionListItem. Add realization method ngOnInit in component HiveSectionListComponent. Add implementation method getHiveSections in srvice hive.service. Add list sections in component HiveSectionListComponent.

--- KatlaSportNg/src/app/hive-management/models/hive-section-list-item.ts
+++ KatlaSportNg/src/app/hive-management/models/hive-section-list-item.ts
@@ -1,4 +1,11 @@
 export class HiveSectionListItem {
     constructor(
+        public id : Int16Array,
+
+        public name : string,
+
+        public code : string,
+
+        public isDeleted : boolean
     ) { }
 }
